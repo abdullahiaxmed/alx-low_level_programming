51153bf7ee887efcbda0f94540048e270fb8ec3a
abdullahiaxmed <abdullahiaxme1002@gmail.com>
2023-02-16 06:29:03 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <studio.>
+
+/**
+ * main .prints "Programming is like building a multilingual puzzle,followe by a new line.
+ * Return:Always 0.
+ */
+
+
+int main(void) 
+
+
+{
+	puts("/"Programming is like building a multilingul puzzle");
+
+
+	return (0);
+
+
+}
